077b7b7ce5cea8176e92e8e20977dd6fede2c949
Illia Plotnykov <ilyaplotnykov@gmail.com>
2026-07-06 14:39:09 +0300

    List of currencies by default settings

--- currencies_classes/file_reader.py
+++ currencies_classes/file_reader.py
@@ -3,7 +3,7 @@ import pandas as pd
 class FileReader:
     @staticmethod
     def get_currency_list(): #Create Currencies List
-        df = ["USD", "EUR", "UAH","GBP"]
+        df = ["USD", "EUR", "PLN","GBP"]
         return df
         #df = pd.read_excel('D:\\Programs\\all_python_projects\\Сurrency_Exchange\\config\\list_currencies.xlsx') #Лист валют з ексель файлу
         #return df['currencies'].tolist()
